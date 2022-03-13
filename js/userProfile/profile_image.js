9669cfaece5fc10dbfedf302424ba01268de0409
karl055 <parolemark@gmail.com>
2022-03-13 16:26:24 +0800

    User Profile Updated

---
+++ js/userProfile/profile_image.js
@@ -0,0 +1,18 @@
+const imgDiv = document.querySelector('.profile_picture');
+const image = document.querySelector('#photo');
+const file = document.querySelector('#file');
+const uploadBtn = document.querySelector('#uploadBtn');
+
+file.addEventListener('change', function(){
+    const photoChose = this.files[0];
+
+    if(photoChose){
+        const reader = new FileReader();
+
+        reader.addEventListener('load', function(){
+            image.setAttribute('src', reader.result);
+        });
+
+        reader.readAsDataURL(photoChose);
+    }
+});
