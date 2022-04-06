846315f95e962eed0176be6e4e411534bb239cf4
Karl055 <parolemark@gmail.com>
2022-04-06 23:47:27 +0800

    Added Terms and Conditions

--- terms_condition.php
+++ terms_condition.php
@@ -8,13 +8,14 @@
         <link rel="icon" href="./assets/img/tab_icon.png">
         <style>
             .terms_content{
-                margin-top: 2rem;
+                margin-top: 1rem;
                 margin-bottom: 2rem;
                 border: 2px solid #e3e3e3;
                 border-radius: 5px;
             }
             .terms_content .container_title{
                 padding-top: 20px;
+                padding-bottom: 1rem;
             }
             .terms_content .terms_body .list_content{
                 padding: 15px 30px 15px;
@@ -24,16 +25,41 @@
             .terms_content .terms_body{
                 padding: 10px 0 0;
             }
+            
+            .backbtn{
+                padding: 1rem;
+            }
+            .backbtn a{
+                color: black;
+                text-decoration: none;
+            }
+            .backbtn a:hover{
+                color: #5e0cc4;
+                text-decoration: none;
+            }
+            .backbtn .backIcon{
+                margin-top: auto;
+                margin-bottom: auto;
+                
+            }
         </style>
         
         <link rel="stylesheet" href="./css/homepage.css">
     </head>
     <body>
         <?php include_once './navbar.php';?>
+        <div class="container">
+            <div class="backbtn" style="display: flex;">
+                <svg class="backIcon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                </svg>    
+                <a href="./homepage.php">Back to Homepage</a>
+            </div>
+        </div>
         <div class="container terms_content">
             <div class="col-12">
                 <div class="col-12 container_title">
-                    <h3>Terms of Service (ETS)</h3>
+                    <h3>Connserv: Terms and Condition</h3>
                 </div>
             </div>
             <div class="col-12 terms_body">
