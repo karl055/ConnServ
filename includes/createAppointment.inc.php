b9c58c9c2c68f5be69577ec5b7fab5d59d93bdda
Karl055 <parolemark@gmail.com>
2022-04-14 16:59:35 +0800

    Fixed backend

--- includes/createAppointment.inc.php
+++ includes/createAppointment.inc.php
@@ -31,64 +31,32 @@ if(isset($_POST['addAppointment'])){
     $clientCity = $_POST['city'];
     
 
+    $char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    $char = str_shuffle($char);
+    for($i = 0, $rand = '', $l = strlen($char) - 1; $i < 3; $i ++) {
+        $rand .= $char{mt_rand(0, $l)};
+    }
+
+    $shufNum = rand(1111, 9999);
+
+    $first = "CON";
+    $third = "VI";
+    $date = date('ymd');
+    $time = time();
+
+    $combination = $first."-".$date.$shufNum."-".$third."-".$time."-".$rand;
+
     
-    $appointmentSql = "INSERT INTO appointment_tb SET business_id = (SELECT business_id FROM business_tb WHERE business_icon = '$businessImg'), client_id = (SELECT user_identity FROM user_tb WHERE user_identity = '$clientId'), client_firstname = '$clientFName', client_lastname = '$clientLname',
+    $appointmentSql = "INSERT INTO appointment_tb SET appointment_custom = '$combination',business_id = (SELECT business_id FROM business_tb WHERE business_icon = '$businessImg'), client_id = (SELECT user_identity FROM user_tb WHERE user_identity = '$clientId'), client_firstname = '$clientFName', client_lastname = '$clientLname',
     min_date = '$preferredMinDate', max_date = '$preferredMaxDate', hour_time = '$preferredHourTime', mins_time = '$preferredMinsTime', hour_clock = '$preferredTime', service_note = '$description',
     business_name = '$businessName', business_email = '$businessEmail', mobile = '$clientMobile', landline = '$clientLandline',
     houseNum = '$clientHouse', street = '$clientStreet', barangay = '$clientBarangay', city = '$clientCity'";
     
     if($appointmentResult = mysqli_query($connect, $appointmentSql)){
 
-        $secondsql = "SELECT * FROM appointment_tb WHERE client_id = '$clientId' AND (SELECT MAX(timedate_stamp) FROM appointment_tb);";
-        $secondresult = mysqli_query($connect, $secondsql);
-        if($row = mysqli_fetch_assoc($secondresult)){
-            
-
-            $char = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            $char = str_shuffle($char);
-            for($i = 0, $rand = '', $l = strlen($char) - 1; $i < 3; $i ++) {
-                $rand .= $char{mt_rand(0, $l)};
-            }
-
-            $shufNum = rand(1111, 9999);
-
-            $first = "CON";
-            $second = $row['appointment_id'];
-            $third = "VI";
-            $date = date('ymd');
-            $time = time();
-
-            $combination = $first."-".$date.$shufNum."-".$third."-".$time."-".$rand;
-
-            $customSql = "INSERT INTO appointmentid_tb SET customId = '$combination', appointmentId = (SELECT appointment_id FROM appointment_tb WHERE appointment_id = '$second')";
-            if($customResult = mysqli_query($connect, $customSql)){
-                header("Location: ../appointment_confirmation.php?simg=$businessImg&sname=$businessName&semail=$businessEmail&cfname=$clientFName&clname=$clientLname&cmobile=$clientMobile&clandline=$clientLandline&chouse=$clientHouse&cstreet=$clientStreet&cbrgy=$clientBarangay&ccity=$clientCity&mindate=$preferredMinDate&maxdate=$preferredMaxDate&hour=$preferredHourTime&mins=$preferredMinsTime&time=$preferredTime&description=$description");
-            }
-            else{
-                echo mysqli_error($connect);
-            }
-        }
-        else{
-            echo mysqli_error($connect);
-        }
+        header("Location: ../appointment_confirmation.php?bimg=$businessImg&cid=$clientId&bname=$businessName&bemail=$businessEmail&cfname=$clientFName&clname=$clientLname&cmobile=$clientMobile&clandline=$clientLandline&prefmindate=$preferredMinDate&prefmaxdate=$preferredMaxDate&prefhourtime=$preferredHourTime&prefminstime=$preferredMinsTime&preftime=$preferredTime&desc=$description&chouse=$clientHouse&cstreet=$clientStreet&cbrgy=$clientBarangay&ccity=$clientCity");
     }
     else{
         echo mysqli_error($connect);
     }
-
-
-    
-    /* 
-    echo $findingRow['business_name']."<br>";
-
-    echo $preferredMinDate." - ".$preferredMaxDate."<br>";
-
-    if(strlen($preferredMinsTime)<2){
-        
-        echo $preferredHourTime.": 0".$preferredMinsTime." ".$preferredTime."<br>";
-    }
-    else{
-        echo $preferredHourTime.": ".$preferredMinsTime." ".$preferredTime."<br>";
-    }
-    echo $description; */
 }
