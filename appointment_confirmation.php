c6d5318ebbf86a6b431a9b0d955474f30cbeddfa
Karl055 <parolemark@gmail.com>
2022-04-22 17:35:20 +0800

    ADDED

--- appointment_confirmation.php
+++ appointment_confirmation.php
@@ -72,17 +72,16 @@ $detailRows = mysqli_fetch_assoc($detailResult);
             <form action="./appointment_confirmation.php" method="post">
                 <div class="rate_content col-12">
                     <div class="rate col-7">
-                    <input type="radio" id="star5" name="rate" value="5">
-                    <label for="star5" title="Great">5 stars</label>
-                    <input type="radio" id="star4" name="rate" value="4">
-                    <label for="star4" title="Awesome">4 stars</label>
-                    <input type="radio" id="star3" name="rate" value="3">
-                    <label for="star3" title="Fantastic">3 stars</label>
-                    <input type="radio" id="star2" name="rate" value="2">
-                    <label for="star2" title="Better">2 stars</label>
-                    <input type="radio" id="star1" name="rate" value="1">
-                    <label for="star1" title="Good">1 star</label>
-                    <button type="submit">Submit</button>
+                        <input type="radio" id="star5" name="rate" value="5">
+                        <label for="star5" title="Great">5 stars</label>
+                        <input type="radio" id="star4" name="rate" value="4">
+                        <label for="star4" title="Awesome">4 stars</label>
+                        <input type="radio" id="star3" name="rate" value="3">
+                        <label for="star3" title="Fantastic">3 stars</label>
+                        <input type="radio" id="star2" name="rate" value="2">
+                        <label for="star2" title="Better">2 stars</label>
+                        <input type="radio" id="star1" name="rate" value="1">
+                        <label for="star1" title="Good">1 star</label>
                     </div>
                     <?php echo $_POST['rate'];?>
                 </div>
